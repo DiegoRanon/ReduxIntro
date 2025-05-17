4d891a974813f893e2e8b1e94e30637e6d5bf07c
Konan <mastercelis8891@gmail.com>
2025-05-17 16:33:26 -0400

    State slices (Customers and account)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
